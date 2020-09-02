b98651fc62d70f75e90cacae7ed478449c96007b
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-09-02 09:38:45 -0300

    using eager relation instead of joining

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -26,7 +26,7 @@ class Transaction {
   @Column('number')
   category_id: string;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -15,9 +15,7 @@ const upload = multer(uploadConfig);
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
 
-  const transactions = await transactionsRepository.find({
-    relations: ['category'],
-  });
+  const transactions = await transactionsRepository.find();
 
   const balance = await transactionsRepository.getBalance();
 
